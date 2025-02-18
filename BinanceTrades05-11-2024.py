5a59930233e5c2df252f3099fd529bea83e2fbdc
ViktorsAlohins <spyxed@gmail.com>
2025-02-18 23:59:19 +0200

    comments added

--- BinanceTrades05-11-2024.py
+++ BinanceTrades05-11-2024.py
@@ -13,16 +13,17 @@ limit = 1000
 
 updatedTime = startTime
 updatedTimesArr = []
-#the step is one minute in milliseconds
+#the step to call the API is one minute in milliseconds
 timeStep = 60 * 1000
 
-#update the time for one minute untill it reaches the endTime
+#update the time for one minute until it reaches the endTime
 while updatedTime < endTime: 
     updatedTimesArr.append((updatedTime, min(updatedTime + timeStep, endTime)))
     updatedTime += timeStep
 
 async def get_rounds_per_step(session, step_start, step_end):
 
+    #list to store all the trades
     trades = []
     start = step_start
 
@@ -34,21 +35,28 @@ async def get_rounds_per_step(session, step_start, step_end):
             "limit" : limit
         }
 
+        #asynchronous request to fetch data from the API
         async with session.get(apiUrl, params=params) as response:
             data = await response.json()         
             if not data: 
                 break
-
+            
+            #check if data in response is in the expected format
             if not isinstance(data, list):
                 print("Unexpected data format:", type(data), data)
                 break
+            #add data to the list
             trades.extend(data)
+            
+            #if the data returned is less than the limit (1000) break the loop
             if len(data) < limit:
                 break
-
+            
+            #update the start time of the next step by one millisec after the Timestamp of the last transaction
             start = data[-1]["T"] + 1
             await asyncio.sleep(0.1)
 
+    #printing the amount of trades fetched for each minute
     start_date = datetime.fromtimestamp(step_start / 1000, timezone.utc)
     end_date = datetime.fromtimestamp(step_end / 1000, timezone.utc)
     print(f"For period {start_date.strftime('%Y-%m-%d %H:%M:%S')} to {end_date.strftime('%Y-%m-%d %H:%M:%S')} : {len(trades)} trades fetched")
@@ -57,29 +65,35 @@ async def get_rounds_per_step(session, step_start, step_end):
 
 async def main():
 
+    #list of all trades fetched
     all_trades = []
 
+    #ste up a connection with a limit of 10 concurrent connections asynchronously
     conn = aiohttp.TCPConnector(limit=10)
     async with aiohttp.ClientSession(connector=conn) as session:
-        events = []
+        events = [] 
         for steps in updatedTimesArr:
             event = get_rounds_per_step(session, steps[0], steps[1])
             events.append(event)
 
+        #gather all the results from the asynchronous tasks
         results = await asyncio.gather(*events)
 
+        # put the trades from all the steps into one list
         for result in results:
             trades = result[2]
             all_trades.extend(trades)
 
         return all_trades
 
+#track the timing of the execution
 start = time.time()
 all_trades = asyncio.run(main())
 end = time.time()
 
 print(f"\nFetched a total of {len(all_trades)} trades in {int(end - start)} seconds.")
 
+#working with data
 df = pd.DataFrame(all_trades)
 df["price"] = df["p"].astype(float)
 df["quantity"] = df["q"].astype(float)
